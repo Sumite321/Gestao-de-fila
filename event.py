c5524ef72d97378e3eed7059641a6396bd9daace
Sumite Ramchande <admin@Sumites-MacBook-Pro.local>
2018-06-13 16:50:20 +0100

    Ler automaticamente

--- event.py
+++ event.py
@@ -1,14 +1,33 @@
-from Tkinter import *
+import Tkinter as tk
+import random
 
-root = Tk()
-sv = StringVar()
+COLORS =["red", "orange", "yellow", "green", "blue", "violet"]
 
-def callback():
-    print(sv.get())
-    return True
+class Application(tk.Frame):
+    
+    def __init__(self,master):
+        self.master = master
+        tk.Frame.__init__(self)
+        self.pack()
+        
+        self._after_id = None
+        self.entry = tk.Entry(self)
+        self.entry.pack()
+        self.entry.bind('<Key>',self.handle_wait)
+        self.entry.focus()
+    def handle_wait(self,event):
+        # cancel the old job
+        if self._after_id is not None:
+            self.after_cancel(self._after_id)
+        
+        # create a new job
+        self._after_id = self.after(1000, self.change_color)
+    
+    def change_color(self):
+        random_color = random.choice(COLORS)
+        self.entry.config(background=random_color)
+        print("butao")
 
-e = Entry(root, textvariable=sv, validate="focusout", validatecommand=callback)
-e.grid()
-e = Entry(root)
-e.grid()
-root.mainloop()
+root = tk.Tk()
+app = Application(root)
+app.mainloop()
