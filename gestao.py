c5aaaf5d9d62d1e32724a80de84e98188b0645e7
Sumite Ramchande <admin@Sumites-MacBook-Pro.local>
2018-06-22 16:27:25 +0100

    Butao

--- gestao.py
+++ gestao.py
@@ -3,6 +3,19 @@ import Tkinter as tk
 import MySQLdb
 import random
 from PIL import ImageTk, Image
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    input_state = GPIO.input(18)
+    if input_state == False:
+        destroy()
+        time.sleep(0.2)
+
 
 
 global a
